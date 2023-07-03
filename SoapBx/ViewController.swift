11aa2ebb13bd989c59c09234caa8061fe1790f83
Harsh Kadiya <harsh.kadiya@fortytwo42.in>
2023-07-03 15:39:32 +0530

    Initial Commit

---
+++ SoapBx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoapBx
+//
+//  Created by Mac on 03/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
